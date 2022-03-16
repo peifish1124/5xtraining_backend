f20776f3f7b09fd1320badfa1c3fb468209918d1
peifish1124 <b08705026@ntu.edu.tw>
2022-03-16 23:18:46 +0800

    fix migration

--- db/migrate/20220316043218_remove_user_id_from_users.rb
+++
@@ -1,8 +0,0 @@
-class RemoveUserIdFromUsers < ActiveRecord::Migration[7.0]
-  def change
-    change_table :users do |t|
-      t.remove :user_id
-      t.rename :user_name, :name
-    end
-  end
-end
